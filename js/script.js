ae6f170936b65b9b35b7ad338c6c1cf87d774e8e
Katie Maxwell <katiemaxwell10@hotmail.com>
2018-08-15 21:39:43 +0200

    styling to events page

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@ $('#countdown').timeTo({
     fontSize: 28,
     captionSize: 12
 });
+
+
+// var block = false;
+// $(".card").mouseenter(function(){
+//     if(!block) {
+//         block = true;
+//         $(".hidden_info").slideDown(400, function(){
+//             block = false;
+//         });
+//     }
+// });
+// $(".card").mouseleave(function(){
+//     if(!block) {
+//         block = true;
+//         $(".hidden_info").slideUp(400, function(){
+//             block = false;
+//         });
+//     }
+// });
+
+// $(".card").mouseenter(function(){
+//     $(".card_img").hide()
+// })
+// $(".card").mouseleave(function(){
+//     $(".card_img").show()
+// })
