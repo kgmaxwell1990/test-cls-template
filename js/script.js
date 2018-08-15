a95196f4bc73a918ab53c60dbe8af6e8fe28532d
Katie Maxwell <katiemaxwell10@hotmail.com>
2018-08-15 17:29:45 +0200

    updated home page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('#countdown').timeTo({
+    timeTo: new Date(new Date('Fri Feb 22 2019 09:00:00 GMT-0800 (Central European Summer Time)')),
+    displayDays: 3,
+    theme: "black",
+    displayCaptions: true,
+    fontSize: 28,
+    captionSize: 12
+});
